c94a5a9cc06f76798a13f0c5f679e6225cf32b35
yaKim <dusdk3288>
2022-04-14 13:05:39 +0900

    소스 정리

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -2,45 +2,11 @@
  * @/store/modules
  */
 
-import auth from '@/store/modules/auth/'
-import commonCode from '@/store/modules/commonCode/'
-import dialog from '@/store/modules/dialog/'
-import keypress from '@/store/modules/keypress/'
-import naver from '@/store/modules/naver/'
-import message from '@/store/modules/message/'
-import partner from '@/store/modules/partner/inventory'
-import settings from '@/store/modules/settings/'
-import sidebar from '@/store/modules/sidebar/'
-import social from '@/store/modules/social/statistics'
-
-const modules = {
-  auth,
-  commonCode,
-  dialog,
-  keypress,
-  naver,
-  message,
-  partner,
-  settings,
-  sidebar,
-  social
-}
-
-/* const modulesFiles = import.meta.globEager('./!**!/index.js')
+const modulesFiles = import.meta.globEager('./**/index.js')
 const modules = {}
 for (const path in modulesFiles) {
   const moduleName = path.replace(/(\.\/|\/index.js|\/[\w\W]+\/index.js)/g, '')
   modules[moduleName] = modulesFiles[path].default
-} */
-
-// FIXME vite require 지원 불가..
-/* 기존 API 소스
-const requireModule = require.context('.', true, /\/[\w\W]+\/index.js$/)
-const modules = {}
-requireModule.keys().forEach(fileName => {
-  const moduleName = fileName.replace(/(\.\/|\/index.js)/g, '')
-  modules[moduleName] = requireModule(fileName).default
-})
-*/
+}
 
 export default modules
