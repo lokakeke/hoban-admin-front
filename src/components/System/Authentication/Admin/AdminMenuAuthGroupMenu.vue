814106b0bbda0207817d19520af6766e22a2d639
yaKim <dusdk3288>
2022-04-26 11:14:43 +0900

    관리자 메뉴 권한 수정

--- src/components/System/Authentication/Admin/AdminMenuAuthGroupMenu.vue
+++ src/components/System/Authentication/Admin/AdminMenuAuthGroupMenu.vue
@@ -9,8 +9,7 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col sm="6" md="3" v-for="menu of menuList" :key="menu.menuId"
-             class="border-bottom border-left border-top border-right">
+      <v-col sm="6" md="3" v-for="menu of menuList" :key="menu.menuId" class="border-bottom border-left border-top border-right">
         <v-row align="center">
           <v-col cols="12" class="headline font-weight-bold text-center px-7">
             <v-icon class="float-left" @click="clickIcon(menu.menuId)">{{checkIcon(menu.menuId)}}</v-icon>
@@ -35,15 +34,16 @@
                   <v-list-item-title :class="{ 'strike': sub.useYn === 'N' }">{{sub.menuName}}</v-list-item-title>
                 </v-list-item-content>
                 <v-list-item-action @click.stop="">
-                  <v-switch v-model="menuWrite" inset dense :value="sub.menuId" label="쓰기 권한" color="info" @change="checkWrite(sub.menuId)"></v-switch>
+                  <v-switch v-model="menuWrite" inset dense :value="sub.menuId" label="쓰기 권한" color="info" @change="checkWrite(sub.menuId, subItem)"></v-switch>
                 </v-list-item-action>
               </v-list-item>
             </template>
             <template v-else>
-              <v-list-item :key="subItem.menuId" @click="check(subItem.menuId)">
-                <v-list-item-action>
-                  <v-checkbox v-model="myMenu" :value="subItem.menuId" @click.prevent.stop=""></v-checkbox>
-                </v-list-item-action>
+              <v-list-item :key="subItem.menuId" @click="checkParents(subItem.menuId)">
+                <v-list-item-icon>
+                  <v-icon>subdirectory_arrow_right</v-icon>
+                  <v-icon :color="myMenu.includes(subItem.menuId) ? '#ee7500' : 'gray'">{{ myMenu.includes(subItem.menuId) ? 'mdi-checkbox-marked' : 'mdi-checkbox-blank-outline' }}</v-icon>
+                </v-list-item-icon>
                 <v-list-item-content :class="{ 'strike': subItem.useYn === 'N' }">
                   {{subItem.menuName}}
                 </v-list-item-content>
@@ -146,6 +146,10 @@ export default {
               if (this.myMenu.indexOf(mId) < 0) {
                 this.myMenu.push(mId)
                 this.myMenu.sort((a, b) => a - b)
+                if (this.menuWrite.indexOf(mId) < 0) {
+                  this.menuWrite.push(mId)
+                  this.menuWrite.sort((a, b) => a - b)
+                }
               }
             }
           }
@@ -156,7 +160,7 @@ export default {
       let menuList = []
       for (const menu of menus) {
         if (menu.children && menu.children.length > 0) {
-          menuList = menuList.concat(this.getMenuList(menu.children))
+          menuList = menuList.concat(menu.menuId, this.getMenuList(menu.children))
         } else {
           menuList.push(menu.menuId)
         }
@@ -183,7 +187,6 @@ export default {
       if (this.selectMenu && this.selectMenu.length > 0) {
         this.myMenu = this.getSelectMenuList(this.menuList, true) // _.map(this.selectMenu.slice(), 'menuId')
         this.menuWrite = this.getSelectMenuList(this.menuList, false) // _.map(_.filter(this.selectMenu.slice(), { writeYn: 'Y' }), 'menuId')
-        console.log(this.myMenu)
       } else {
         this.myMenu = []
         this.menuWrite = []
@@ -191,7 +194,7 @@ export default {
     },
     checkChild (parentMenu, menuId) {
       // 클릭한 child menu id check 태움
-      this.check(menuId)
+      this.checkParents(menuId)
       if (parentMenu.menuId && parentMenu.children && parentMenu.children.length > 0) {
         // child menu 중 myMenu 에 포함되어 있으면 this.myMenu 에 부모 메뉴 추가 아니면 삭제
         const childrenMenuIdArray = parentMenu.children.map(row => row.menuId)
@@ -201,15 +204,22 @@ export default {
           if (!_.includes(this.myMenu, parentMenuId)) {
             this.myMenu.push(parentMenuId)
             this.myMenu.sort((a, b) => a - b)
+            if (this.menuWrite.indexOf(parentMenuId) < 0) {
+              this.menuWrite.push(parentMenuId)
+              this.menuWrite.sort((a, b) => a - b)
+            }
           }
         } else {
           if (_.includes(this.myMenu, parentMenuId)) {
             this.myMenu.splice(this.myMenu.indexOf(parentMenuId), 1).sort((a, b) => a - b)
+            if (this.menuWrite.indexOf(parentMenuId) > -1) {
+              this.menuWrite.splice(this.menuWrite.indexOf(parentMenuId), 1).sort((a, b) => a - b)
+            }
           }
         }
       }
     },
-    check (menuId) {
+    checkParents (menuId) {
       if (_.includes(this.myMenu, menuId)) {
         this.myMenu.splice(this.myMenu.indexOf(menuId), 1).sort((a, b) => a - b)
         if (this.menuWrite.indexOf(menuId) > -1) {
@@ -218,16 +228,26 @@ export default {
       } else {
         this.myMenu.push(menuId)
         this.myMenu.sort((a, b) => a - b)
+        if (this.menuWrite.indexOf(menuId) < 0) {
+          this.menuWrite.push(menuId)
+          this.menuWrite.sort((a, b) => a - b)
+        }
       }
     },
-    checkWrite (menuId) {
+    checkWrite (menuId, parentMenu) {
       // 메뉴 권한 체크가 되어있는지 확인한다.
       if (!_.includes(this.myMenu, menuId)) {
-        this.check(menuId)
+        if (parentMenu && menuId) {
+          this.checkChild(parentMenu, menuId)
+        } else {
+          this.checkParents(menuId)
+        }
       }
     },
     setMenu () {
       this.$dialog.confirm('메뉴권한그룹(' + this.menuAuthGroupName + ')의<br/>메뉴를 적용 하시겠습니까?').then(() => {
+        this.myMenu.sort((a, b) => a - b)
+        this.menuWrite.sort((a, b) => a - b)
         // 메뉴 권한 리스트를 작성
         const array = []
         for (const menuId of this.myMenu) {
