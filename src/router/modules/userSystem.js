5e232590a7a6eae6a6c707247b343ba4915d42d6
LeeBJ <realbbeng@byulsoft.com>
2022-08-03 16:13:38 +0900

    사용자 시스템 메뉴 추가(user-system)

---
+++ src/router/modules/userSystem.js
@@ -0,0 +1,41 @@
+import Full from '@/container/Full.vue'
+
+// Package
+const Package = () => import('@/views/userSystem/package/PackageManagement.vue')
+
+// Payment
+const Payment = () => import('@/views/userSystem/payment/PaymentManagement.vue')
+
+export default {
+  path: '/user/system',
+  name: 'userSystem',
+  component: Full,
+  redirect: '/main',
+  meta: {
+    breadcrumb: '사용자'
+  },
+  children: [
+    {
+      path: 'package',
+      component: Package,
+      name: 'package',
+      meta: {
+        auth: true,
+        requiresAuth: true,
+        title: '패키지 관리',
+        breadcrumb: 'Package '
+      }
+    },
+    {
+      path: 'payment',
+      component: Payment,
+      name: 'payment',
+      meta: {
+        auth: true,
+        requiresAuth: true,
+        title: '결제 관리',
+        breadcrumb: 'Payment '
+      }
+    }
+  ]
+}
---
+++ src/views/userSystem/package/PackageManagement.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <v-row v-for="(item, index) of list" :key="index">
+            <v-col cols="2">
+                {{ item.packageNo }}
+            </v-col>
+            <v-col cols="2">
+                {{ item.packageName }}
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+import packageService from '@/api/modules/userSystem/package.service'
+export default {
+  name: 'PackageManagement',
+  data () {
+    return {
+      list: []
+    }
+  },
+  mounted () {
+    this.selectPackageList()
+  },
+  methods: {
+    selectPackageList () {
+      // 테스트 조회
+      packageService.selectPackageList().then(res => {
+        console.log(res)
+        this.list = res.data
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
